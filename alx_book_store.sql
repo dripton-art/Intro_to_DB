1a17ee108e0554b58515db9b0de67c8f8a226eae
ivan <ivankaweesa.biz@gmail.com>
2025-07-04 02:42:48 +0200

    A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,7 +5,7 @@ USE alx_book_store;
 
 --create books schema
 CREATE TABLE Books (
-    book_id (Primary Key),
+    book_id Primary Key,
     title VARCHAR(130),
     author_id (Foreign Key referencing Authors table),
     price DOUBLE,
@@ -14,13 +14,13 @@ CREATE TABLE Books (
 
 --create Authors table schema
 CREATE TABLE Authors (
-    author_id (Primary Key),
+    author_id Primary Key,
     author_name VARCHAR(215)
 );
 
 --create Customers table schema
 CREATE TABLE Customers (
-    customer_id (Primary Key),
+    customer_id Primary Key,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
@@ -28,14 +28,14 @@ CREATE TABLE Customers (
 
 --create Orders table schema
 CREATE TABLE Orders (
-    order_id (Primary Key),
+    order_id Primary Key,
     customer_id (Foreign Key referencing Customers table),
     order_date DATE
 );
 
 --create Order_Details
 CREATE TABLE Order_Details (
-    orderdetailid (Primary Key),
+    orderdetailid Primary Key,
     order_id (Foreign Key referencing Orders table),
     book_id (Foreign Key referencing Books table),
     quantity DOUBLE
