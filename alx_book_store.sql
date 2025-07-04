d04e94aaa9fde79085b5ced1f4f042f97760b664
ivan <ivankaweesa.biz@gmail.com>
2025-07-04 03:12:32 +0200

    A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -39,6 +39,7 @@ CREATE TABLE Order_Details (
     orderdetailid PRIMARY KEY,
     order_id INT
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
