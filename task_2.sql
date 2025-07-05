d3f2d76624c57d8a653fde954125f3169e90a6ec
ivan <ivankaweesa.biz@gmail.com>
2025-07-05 21:51:51 +0200

    Magical Tables

---
+++ task_2.sql
@@ -0,0 +1,42 @@
+
+USE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id PRIMARY KEY,
+    author_id INT,
+    title VARCHAR(130),
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+--create Customers table schema
+CREATE TABLE Customers (
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+--create Orders table schema
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+--create Order_Details
+CREATE TABLE Order_Details (
+    orderdetailid PRIMARY KEY,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
